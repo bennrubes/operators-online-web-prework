28db2fc26a85ca8be64ab3041c8fc79b1b1c1c1e
Sam Bennrubi <sbennrubi@gmail.com>
2018-10-29 04:08:39 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
 def unsafe?(speed)
-  if speed < 40 || speed > 60 ? unsafe : safe
+  if speed < 40 || speed > 60
+    unsafe
+  else
+    safe
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ?  not_safe : safe
 end
 	
 
